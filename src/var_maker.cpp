19e17e8b5382ed4c09ba88c3f92b26846a3ba619
Jordy Ruiz <jordyruiz@hotmail.fr>
2017-01-19 17:52:22 +0100

    Added missing var_maker.cpp file

---
+++ src/var_maker.cpp
@@ -0,0 +1,19 @@
+#include "var_maker.h"
+
+/**
+ * @fn void VarMaker::transfer(VarMaker& vm);
+ * @brief      Transfer all tops from a vm to this, and clear the vm
+ * @param      vm    The var maker to use
+ */
+
+/**
+ * @fn const Operand* VarMaker::new_top(void);
+ * @brief      Make a new Tk
+ * @return     The operand generated
+ */
+
+/**
+ * @fn void scale(int offset);
+ * @brief      Scale all the Tk (increase k by an offset)
+ * @param[in]  offset  The offset to add to the var identifiers
+ */
